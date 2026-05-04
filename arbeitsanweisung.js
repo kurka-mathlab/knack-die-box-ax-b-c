7d8b2e6fcbf42969f919ed4da34a1428b6dc30f8
kurka-mathlab <kisselsabine@googlemail.com>
2026-05-04 21:12:16 +0200

    Initial commit

---
+++ arbeitsanweisung.js
@@ -0,0 +1,3 @@
+const instructionText = `Vor dir auf dem Tisch liegen Streichhölzer und Streichholzboxen. Der Tisch ist durch eine schwarze Trennwand in zwei Hälften geteilt. <strong>Auf beiden Seiten der Trennwand befinden sich die gleiche Anzahl an Streichhölzern</strong>, manche Streichhölzer sind in den Boxen versteckt (in jeder Box sind gleich viele Hölzchen). 
+<br><strong>Finde heraus, wie viele Streichhölzer in einer Box sein müssen.</strong> 
+<br><strong>Tippe</strong> auf die Streichhölzer und Boxen, um sie abzulegen.`;
---
+++ aufgaben.js
@@ -0,0 +1,7 @@
+const aufgaben = {
+  1: { leftBoxes: 1, leftMatches: 2, rightBoxes: 0, rightMatches: 6 },
+  2: { leftBoxes: 2, leftMatches: 0, rightBoxes: 0, rightMatches: 6 },
+  3: { leftBoxes: 1, leftMatches: 3, rightBoxes: 0, rightMatches: 10 },
+  4: { leftBoxes: 1, leftMatches: 5, rightBoxes: 0, rightMatches: 6 },
+  5: { leftBoxes: 3, leftMatches: 0, rightBoxes: 0, rightMatches: 9 }
+};
