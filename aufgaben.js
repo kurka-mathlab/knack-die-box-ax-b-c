ec8dba22e44ff9905456388822e326d9e42c1e38
kurka-mathlab <kisselsabine@googlemail.com>
2026-05-07 20:35:56 +0200

    Swap values of tasks 1 and 2 in aufgaben.js

--- aufgaben.js
+++ aufgaben.js
@@ -1,6 +1,6 @@
 const aufgaben = {
-  1: { leftBoxes: 3, leftMatches: 2, rightBoxes: 0, rightMatches: 8 },
-  2: { leftBoxes: 2, leftMatches: 3, rightBoxes: 0, rightMatches: 9 },
+  1: { leftBoxes: 2, leftMatches: 3, rightBoxes: 0, rightMatches: 9 },
+  2: { leftBoxes: 3, leftMatches: 2, rightBoxes: 0, rightMatches: 8 },
   3: { leftBoxes: 4, leftMatches: 1, rightBoxes: 0, rightMatches: 5 },
   4: { leftBoxes: 5, leftMatches: 2, rightBoxes: 0, rightMatches: 17 },
   5: { leftBoxes: 2, leftMatches: 5, rightBoxes: 0, rightMatches: 9 }
